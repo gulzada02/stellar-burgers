da20216e2de95da629f76667c2bf6195dff58a4a
Gulzada <gulzada1996@gmail.com>
2026-06-05 17:57:37 +0200

    updated

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -13,12 +13,22 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <NavLink to='/' className={styles.link}>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
           <BurgerIcon type='primary' />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
         </NavLink>
 
-        <NavLink to='/feed' className={styles.link}>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
           <ListIcon type='primary' />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
         </NavLink>
@@ -30,12 +40,19 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         </NavLink>
       </div>
 
-      <NavLink to='/profile' className={styles.link_position_last}>
-        <ProfileIcon type='primary' />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
-      </NavLink>
+      <div className={styles.link_position_last}>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          <ProfileIcon type='primary' />
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </NavLink>
+      </div>
     </nav>
   </header>
 );
